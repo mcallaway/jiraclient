575597c4320deecc90bbda6717314ec6053991e7
Matthew Callaway <mcallawa@genome.wustl.edu>
2010-08-26 16:35:25 -0500

    Support gpfs.  Better support for snmp disk groups.

--- DiskUsage/lib/DiskUsage/SNMP.pm
+++ DiskUsage/lib/DiskUsage/SNMP.pm
@@ -15,7 +15,7 @@ local $| = 1;
 # Add commas to big numbers
 my $comma_rx = qr/\d{1,3}(?=(\d{3})+(?!\d))/;
 # Convention for all NFS exports
-my @prefixes = ("/vol","/home");
+my @prefixes = ("/vol","/home","/gpfs");
 
 # A mapping of disk related OIDs
 my $oids = {
@@ -32,7 +32,10 @@ my $oids = {
     'hrStorageSize'  => '1.3.6.1.2.1.25.2.3.1.5',
     'hrStorageUsed'  => '1.3.6.1.2.1.25.2.3.1.6',
     'extTable'       => '1.3.6.1.4.1.2021.8',
-    'nsExtendOutLine' => '1.3.6.1.4.1.8072.1.3.2.4.1.2',
+    'nsExtendOutput2Table'  => '1.3.6.1.4.1.8072.1.3.2.4',
+    'nsExtendOutLine'       => '1.3.6.1.4.1.8072.1.3.2.4.1.2',
+    'nsExtendOutLine-gpfs'  => '1.3.6.1.4.1.8072.1.3.2.4.1.2.4.103.112.102.115.1',
+    'nsExtendOutLine-group' => '1.3.6.1.4.1.8072.1.3.2.4.1.2.15.100.105.115.107.95.103.114.111.117.112.95.110.97.109.101',
   },
   'netapp'           => {
     'dfFileSys'      => '1.3.6.1.4.1.789.1.5.4.1.2',
@@ -54,6 +57,8 @@ sub new {
   my $class = shift;
   my $self = {
     parent => shift,
+    no_snmp => 0,
+    is_gpfs => undef,
   };  bless $self, $class;
   return $self;
 }
@@ -81,6 +86,8 @@ sub snmp_get_request {
   my $self = shift;
   my $args = shift;
   my $result = {};
+
+  $self->local_debug("snmp_get_request( " . Dumper($args) . ")\n");
   eval {
     $result = $self->{snmp_session}->get_request(-varbindlist => $args );
   };
@@ -104,14 +111,76 @@ sub snmp_get_table {
   return $result;
 }
 
-sub spot_gpfs {
-  # HOST-RESOURCES-MIB::hrSWRunName = gpfsSwapdKproc
+sub spot_gpfsswapd {
   my $self = shift;
-  $self->local_debug("spot_gpfs()\n");
-  my $res = $self->snmp_get_table( $oids->{'gpfs'}->{'processList'} );
+  my $res;
+  $self->local_debug("spot_gpfsswapd\n");
+
+  # This takes a long time because the process list is big.
+  eval {
+    $res = $self->snmp_get_table( $oids->{'gpfs'}->{'processList'} );
+  };
+  if ($@ or length($self->{snmp_session}->error())) {
+    my $msg = $self->{snmp_session}->error();
+    if ($msg =~ /No response/) {
+      $self->logger("took too long looking for gpfs processes...proceeding\n");
+    } elsif ($msg =~ /Message size exceeded/) {
+      my $size = $self->{snmp_session}->max_msg_size();
+      return 0 if ($size == 12000); # don't repeat more than once
+      $self->{snmp_session}->max_msg_size(12000); # try larger msg size once
+      return $self->spot_gpfsswapd();
+    } else {
+      $self->error($self->{snmp_session}->error());
+    }
+  }
   my @processes = values %$res;
   return 1 if grep /gpfsSwapdKproc/, @processes;
   return 0;
+}
+
+sub spot_gpfsext {
+  my $self = shift;
+  my $res;
+
+  $self->local_debug("spot_gpfsext()\n");
+
+  eval {
+    my $oid = $oids->{'linux'}->{'nsExtendOutLine-gpfs'};
+    $res = $self->snmp_get_request( [ $oid ] );
+  };
+  if ($@ or length($self->{snmp_session}->error())) {
+    my $msg = $self->{snmp_session}->error();
+    print Dumper("msg: " . $msg);
+    if ($msg =~ /No response/) {
+      $self->logger("took too long looking for gpfs processes...proceeding\n");
+    } elsif ($msg =~ /Message size exceeded/) {
+      my $size = $self->{snmp_session}->max_msg_size();
+      return 0 if ($size == 12000); # don't repeat more than once
+      $self->{snmp_session}->max_msg_size(12000);
+      return $self->spot_gpfsext();
+    } else {
+      $self->error($self->{snmp_session}->error());
+    }
+  }
+  my $result = pop @{ [ values %$res ] };
+  return 1 if ($result eq 'true');
+  return 0;
+}
+
+sub spot_gpfs {
+  # HOST-RESOURCES-MIB::hrSWRunName = gpfsSwapdKproc
+  my $self = shift;
+
+  return $self->{is_gpfs} if (defined $self->{is_gpfs});
+  $self->local_debug("spot_gpfs()\n");
+  my $result = 0;
+
+  #my $result = $self->spot_gpfsswapd();
+  $result = $self->spot_gpfsext();
+
+  $self->local_debug("is_gpfs: $result\n");
+  $self->{is_gpfs} = $result;
+  return $result;
 }
 
 sub type_string_to_type {
@@ -130,6 +199,7 @@ sub type_string_to_type {
     if ($typestr =~ $regex) {
       my $type = $dispatcher{$regex};
       # Spot gpfs among linux hosts
+      # FIXME: perhaps just use naming convention in mount_path
       if ($type eq 'linux') {
         $type = 'gpfs' if ($self->spot_gpfs());
       }
@@ -168,18 +238,19 @@ sub get_snmp_disk_usage {
 
   $self->local_debug("get_snmp_disk_usage()\n");
 
-  # What is the target
+  # Need to know what sort of host this is to see what SNMP tables to ask for.
   my $host_type = $self->get_host_type();
 
-  # Fetch all volumes on target linux host
+  # Fetch all volumes on target host
   my $ref;
   if ($host_type eq 'netapp') {
+    # NetApp is different than Linux
     $ref = $self->snmp_get_table( $oids->{$host_type}->{'dfFileSys'} );
   } else {
     $ref = $self->snmp_get_table( $oids->{$host_type}->{'hrStorageDescr'} );
   }
 
-  # Iterate over all volume points
+  # Iterate over all volumes and get consumption info.
   foreach my $volume_path_oid (keys %$ref) {
     # Iterate over subset of volumes that we export, based on
     # a naming convention adopted by Systems team.
@@ -191,12 +262,14 @@ sub get_snmp_disk_usage {
 
         # FIXME This is a mess...
 
-        # Create arg list for SNMP
+        # Create arg list for SNMP, what to ask for.
         my @args;
         my @items;
         if ($host_type eq 'netapp') {
+          # NetApps do this
           @items = ('dfHighTotalKbytes','dfLowTotalKbytes','dfHighUsedKbytes','dfLowUsedKbytes');
         } else {
+          # Linux boxes do this
           @items = ('hrStorageUsed','hrStorageSize','hrStorageAllocationUnits');
         }
         foreach my $item (@items) {
@@ -240,7 +313,6 @@ sub get_snmp_disk_usage {
         # Correct for signed 32 bit INTs
         $result->{$ref->{$volume_path_oid}}->{'used_kb'} = $used;
         $result->{$ref->{$volume_path_oid}}->{'total_kb'} = $total;
-
         $result->{$ref->{$volume_path_oid}}->{'physical_path'} = $ref->{$volume_path_oid};
       }
     }
@@ -271,6 +343,49 @@ sub get_mount_point {
   }
 }
 
+sub get_disk_group_via_snmp {
+  my $self = shift;
+  my $physical_path = shift;
+  my $mount_path = shift;
+
+  $self->local_debug("get_disk_group_via_snmp\n");
+
+  # Try SNMP for linux hosts, which may have been configured to
+  # report disk group touch files via SNMP.  Save the result so
+  # we only query SNMP once per host per run.
+  if (! defined $self->{groups}) {
+    eval {
+      my $oid = $oids->{'linux'}->{'nsExtendOutLine-group'};
+      $self->{groups} = $self->snmp_get_table( $oid );
+    };
+    if ($@ or length($self->{snmp_session}->error())) {
+      my $msg = $self->{snmp_session}->error();
+      if ($msg =~ /No response/) {
+        $self->logger("took too long looking for groups via snmp...proceeding\n");
+      } elsif ($msg =~ /Requested table is empty/) {
+        $self->logger("this host doesn't serve groups via snmp...proceeding\n");
+        $self->{no_snmp} = 1;
+      } elsif ($msg =~ /Message size exceeded/) {
+        my $size = $self->{snmp_session}->max_msg_size();
+        return if ($size == 12000); # don't do this twice
+        $self->{snmp_session}->max_msg_size(12000); # try larger size
+        return $self->get_disk_group_via_snmp($physical_path,$mount_path);
+      } else {
+        $self->error($self->{snmp_session}->error());
+      }
+    }
+  }
+  foreach my $touchfile (values %{ $self->{groups} } ) {
+    $touchfile =~ /^(.*)\/(\S+)/;
+    my $dirname = $1;
+    my $group_name = $2;
+    if ($dirname eq $physical_path) {
+      $self->local_debug("snmp says $mount_path belongs to $group_name\n");
+      return $group_name;
+    }
+  }
+}
+
 sub get_disk_group {
   # Look on a mount point for a DISK_ touch file.
 
@@ -290,19 +405,16 @@ sub get_disk_group {
     return $group_name;
   }
 
+  $self->local_debug("no group known for $mount_path\n");
+
   # Determine the disk group name.
   my $host_type = $self->get_host_type();
-  if ($host_type eq 'linux') {
-    # Try SNMP for linux hosts, which may have been configured to
-    # report disk group touch files via SNMP.
-    my $ref = $self->snmp_get_table( $oids->{$host_type}->{'nsExtendOutLine'} );
-    foreach my $touchfile (values %$ref) {
-      $touchfile =~ /^(.*)\/(\S+)/;
-      my $dirname = $1;
-      my $group_name = $2;
-      return $group_name if ($dirname eq $physical_path);
-    }
+  if ($host_type eq 'linux' and ! $self->{no_snmp}) {
+    my $group_name = $self->get_disk_group_via_snmp($physical_path,$mount_path);
+    return $group_name if (defined $group_name);
   }
+
+  $self->local_debug("mount $mount_path and look for touchfile\n");
 
   # This will actually mount a mount point via automounter.
   # Be careful to not overwhelm NFS servers.
@@ -313,7 +425,9 @@ sub get_disk_group {
   } else {
     $group_name = 'unknown';
   }
+
   $self->local_debug("$mount_path is group: $group_name\n");
+
   return $group_name;
 }
 
@@ -321,6 +435,7 @@ sub get_disk_group {
 sub connect_snmp {
   my $self = shift;
   my $host = shift;
+  my $timeout = int($self->{parent}->{timeout});
 
   $self->local_debug("connect_snmp($host)\n");
 
@@ -330,11 +445,16 @@ sub connect_snmp {
      -hostname => $host,
      -community => 'gscpublic',
      -version => '2c',
-     -timeout => 5,
+     -timeout => $timeout,
      -retries => 1,
      -debug => 0x20,
     );
   };
+
+  # SNMP connection debugging
+  #$sess->debug( [ 0x2, 0x4, 0x8, 0x10, 0x20 ] )
+  #  if ($self->{parent}->{debug});
+
   if ($@ or ! defined $sess) {
     $self->error("SNMP failed to connect to host: $host: $err\n");
   }
@@ -342,9 +462,6 @@ sub connect_snmp {
   if (defined $self->{snmp_session}) {
     $self->{snmp_session}->close();
   }
-
-  # Note: We're returning some big messages!
-  $sess->max_msg_size(15000);
 
   $self->{snmp_session} = $sess;
 }
