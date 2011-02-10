0396baf2105ff33d73c26455effd34a4c6e89018
Matt Callaway <mcallawa@genome.wustl.edu>
2011-02-10 14:54:56 -0600

    Add javascript support for RRD graphing.

---
+++ DiskUsage/media/js/rrdgraph.js
@@ -0,0 +1,54 @@
+
+function draw() {
+  document.write('<div id="rrdgraph"></div>');
+  var group_name = unescape(location.search.substr(1)).toLowerCase();
+
+  fname="rrd/" + group_name + ".rrd";
+
+  try {
+    // From binaryXHR.js
+    FetchBinaryURLAsync(fname,update_plot_handler);
+  } catch (err) {
+    alert("Failed loading "+fname+"\n"+err);
+  }
+}
+
+// This is the callback function that,
+// given a binary file object,
+// verifies that it is a valid RRD archive
+// and performs the update of the Web page
+function update_plot_handler(bf) {
+  var i_rrd_data=undefined;
+  try {
+    // From RRDFile.js
+    var i_rrd_data=new RRDFile(bf);
+  } catch(err) {
+    alert("File "+fname+" is not a valid RRD archive!");
+  }
+  if (i_rrd_data!=undefined) {
+    rrd_data=i_rrd_data;
+    update_plot();
+  }
+}
+
+// This function updates the Web Page with the data from the RRD
+function update_plot() {
+  //var gtype_DS=new Array('avail','used');
+
+  var gtype_format={
+    'avail':{
+      title:'avail',label:'avail',color:"#00ff00",lines: {show:true}
+    },
+    'used':{
+      title:'used',label:'used',color:"#aa0000",checked:true
+    },
+  };
+
+  // From rrdFilter.js
+  // select only a subset of DSs to plot
+  //var rrd_data1=new RRDFilterDS(rrd_data,gtype_DS);
+
+  // the rrdFlot object creates and handles the graph
+  //var f=new rrdFlot("rrdgraph",rrd_data1,null,gtype_format);
+  var f=new rrdFlot("rrdgraph",rrd_data,null,gtype_format);
+}
