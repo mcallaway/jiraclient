d9194a6ae02adae874e15f431e5b57e724ab115b
Matt Callaway <mcallawa@genome.wustl.edu>
2011-02-25 10:01:56 -0600

    Move jiraclient to own repo

--- DiskUsage/bin/disk_usage.pl
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env perl
-
-use DiskUsage;
-
-my $app = DiskUsage->new();
-my $rc = $app->main(@ARGV);
-exit $rc;
--- LSFSpool/bin/lsf_spool.pl
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/perl
-
-use LSFSpool;
-my $app = LSFSpool->new();
-my $rc = $app->main(@ARGV);
-exit $rc;
-
