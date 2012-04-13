a42ce1fd198ece70a38e08ab5fa3e4e47c9c5625
Matt Callaway <mcallawa@genome.wustl.edu>
2012-04-13 18:51:41 -0500

    More fixes from testing

--- src/jiraclient.py
+++ src/jiraclient.py
@@ -160,6 +160,20 @@ class Jiraclient(object):
       default=None,
     )
     optParser.add_option(
+      "--jsondata",
+      action="store",
+      dest="jsondata",
+      help="JSON data for use with the API option",
+      default=None,
+    )
+    optParser.add_option(
+      "--method",
+      action="store",
+      dest="method",
+      help="HTTP method for use with the API option",
+      default="get",
+    )
+    optParser.add_option(
       "-c","--comment",
       action="store",
       dest="comment",
@@ -196,10 +210,24 @@ class Jiraclient(object):
       default=None,
     )
     optParser.add_option(
+      "--norcfile",
+      action="store",
+      dest="norcfile",
+      help="Do not parse issue defaults when using --template",
+      default=False,
+    )
+    optParser.add_option(
       "-n","--noop",
       action="store_true",
       dest="noop",
-      help="Parse bug file but don't connect to Jira",
+      help="Do everything locally, never call to the remote API",
+      default=False,
+    )
+    optParser.add_option(
+      "--nopost",
+      action="store_true",
+      dest="nopost",
+      help="Do everything except POST and PUT methods, ie. no creation events",
       default=False,
     )
     optParser.add_option(
@@ -451,7 +479,7 @@ class Jiraclient(object):
         setattr(self.options,k,v)
 
   def read_issue_defaults(self):
-    self.logger.debug("read config %s" % self.options.config)
+    self.logger.debug("read issue defaults %s" % self.options.config)
     parser = ConfigParser.ConfigParser()
     parser.optionxform = str
     try:
@@ -465,14 +493,13 @@ class Jiraclient(object):
       if not hasattr(self.options,k):
         # You can't set in rcfile something that isn't also an option.
         self.fatal("Unknown issue attribute: %s" % k)
-      if getattr(self.options,k) is None:
-        # Take the rc file value if not given on CLI
-        self.logger.debug("set %s %s" % (k,v))
-        setattr(self.options,k,v)
+      self.logger.debug("set %s %s" % (k,v))
+      setattr(self.options,k,v)
 
   def call_api(self,method,uri,payload=None):
     self.logger.debug("Call API: %s %s/%s payload=%s" % (method,self.options.jiraurl,uri,payload))
     if self.options.noop: return {}
+    if self.options.nopost and ( method.lower() == 'post' or method.lower() == 'put' ): return {}
     self.proxy.uri = "%s/%s" % (self.options.jiraurl, uri)
     call = getattr(self.proxy,method)
     headers = {'Content-Type' : 'application/json'}
@@ -496,26 +523,28 @@ class Jiraclient(object):
       return {}
 
   def get_project_id(self,projectKey):
+    if self.maps['project']: return
     uri = "%s/%s" % ('rest/api/latest/project', projectKey)
     data = self.call_api("get",uri)
     self.maps['project'][int(data["id"])] = projectKey.lower()
 
   def get_issue_types(self,projectKey):
+    if self.maps['issuetype']: return
     self.check_auth()
     uri = '''%s?projectKeys=%s''' % ('rest/api/latest/issue/createmeta', projectKey)
     data = self.call_api("get",uri)
     for item in data['projects'][0]['issuetypes']:
-      #self.maps['issuetype'][str(item['name'].lower())] = int(item['id'])
       self.maps['issuetype'][int(item['id'])] = str(item['name'].lower())
 
   def get_resolutions(self):
+    if self.maps['resolutions']: return
     uri = 'rest/api/latest/resolution'
     data = self.call_api("get",uri)
     for item in data:
-      #self.maps['resolutions'][str(item['name'].lower())] = int(item['id'])
       self.maps['resolutions'][int(item['id'])] = str(item['name'].lower()) 
 
   def get_project_versions(self,projectKey):
+    if self.maps['fixversions']: return
     uri = "%s/%s/%s" % ('rest/api/latest/project', projectKey, 'versions')
     data = self.call_api("get",uri)
     for item in data:
@@ -523,17 +552,29 @@ class Jiraclient(object):
     self.maps['fixversions'] = self.maps['versions']
 
   def get_project_components(self,projectKey):
+    if self.maps['components']: return
     uri = "%s/%s/%s" % ('rest/api/latest/project', projectKey, 'components')
     data = self.call_api("get",uri)
     for item in data:
       self.maps['components'][int(item['id'])] = str(item['name'].lower())
 
   def get_priorities(self):
+    if self.maps['priority']: return
     uri = 'rest/api/latest/priority'
     data = self.call_api("get",uri)
     for item in data:
-      #self.maps['priority'][str(item['name'].lower())] = int(item['id'])
       self.maps['priority'][int(item['id'])] = str(item['name'].lower())
+
+  def update_maps_from_jiraserver(self):
+    self.logger.debug("update maps from jira server")
+    # Need project first.
+    # These need to happen before any issue creation or modification
+    self.get_project_id(self.options.project)
+    self.get_issue_types(self.options.project)
+    self.get_project_versions(self.options.project)
+    self.get_project_components(self.options.project)
+    self.get_resolutions()
+    self.get_priorities()
 
   def get_serverinfo(self):
     uri = 'rest/api/latest/serverInfo'
@@ -547,7 +588,7 @@ class Jiraclient(object):
     return self.call_api("get",uri)
 
   def delete_issue(self,issueID):
-    uri = 'rest/api/latest/issue/%s?deleteSubtasks' % issueID
+    uri = 'rest/api/latest/issue/%s?deleteSubtasks=true' % issueID
     self.call_api('delete',uri)
     self.logger.info("Deleted %s/browse/%s" % (self.get_serverinfo()['baseUrl'], issueID))
 
@@ -570,16 +611,22 @@ class Jiraclient(object):
     result = self.call_api('post',uri,payload=comment)
     return result
 
-  def clean_issue(self,issueObj):
+  def clean_issue(self,issue):
     # We have an Issue with a number of required default values
     # that are often empty.  Remove the empty ones so as to not
     # confuse the API service.
-    issue = issueObj.__dict__
+    self.logger.debug("clean issue start: %s" % issue)
+    if type(issue) is not dict:
+      issue = issue.__dict__
     for k,v in issue.items():
       if not v: issue.pop(k)
       if v == {"id":None}: issue.pop(k)
       if v == {"name":None}: issue.pop(k)
       if v == [{"id":None}]: issue.pop(k)
+      if k == self.options.epic_theme_id:
+        if type(v) is not list:
+          issue[k]=[v]
+    self.logger.debug("cleaned issue: %s" % issue)
     return issue
 
   def create_issue(self,issueObj):
@@ -588,28 +635,30 @@ class Jiraclient(object):
     self.logger.debug("payload: %s" % payload)
     uri = 'rest/api/latest/issue'
     newissue = self.call_api('post',uri,payload=payload)
-    issueID = newissue["key"]
+    issueID = "NOOP"
+    if newissue:
+      issueID = newissue["key"]
     self.logger.info("Created %s/browse/%s" % (self.get_serverinfo()['baseUrl'], issueID))
     return issueID
 
   def modify_issue(self,issueID,issueObj):
     issue = self.clean_issue(issueObj)
+    self.logger.debug("modify issue: %s %s" % (issueID,issue))
 
     # FIXME: I'm not sure of a good way to see if we just said --issue with no other options.
     # So this is a hack to check to see if that's the case.
     issuecopy = issue
-    issuecopy.pop("project")
+    if issuecopy.has_key("project"): issuecopy.pop("project")
     if not issuecopy:
       # We specified nothing but --issue, in that case, just display that issue
       # This is like --display --issue FOO-123
       return self.display_issue(issueID)
     payload = json.dumps({"fields":issue})
-    self.logger.debug("payload: %s" % payload)
     uri = 'rest/api/latest/issue/%s' % issueID
     self.call_api('put',uri,payload=payload)
     self.logger.info("Modified %s/browse/%s" % (self.get_serverinfo()['baseUrl'], issueID))
 
-  def get_issue_links(self,issueID,linktype=None):
+  def get_issue_links(self,issueID):
     uri = 'rest/api/latest/issue/%s' % issueID
     data = self.call_api('get',uri)
     return data['fields']['issuelinks']
@@ -670,20 +719,22 @@ class Jiraclient(object):
     #   summary is a string
     #   project is a dict with id
     #   versions is a list of dicts with id
+    self.logger.debug("update issue %s %s %s" % (issue,key,value))
+
+    if not hasattr(issue,key):
+      setattr(issue,key,value)
 
     attr = getattr(issue,key)
 
     # Does the 'key' exist in self.maps to provide an ID?
-    attribute_map = None
-    if self.maps.has_key(key.lower()):
+    if type(value) is int:
+      attribute_id = value
+    elif self.maps.has_key(key.lower()):
       attribute_map = self.maps[key.lower()]
       attribute_id = attribute_map.find_key(value.lower())
       if not attribute_id:
         self.logger.debug("no id found for %s" % (value.lower()))
         return issue
-    else:
-      self.logger.debug("no map found for %s" % (key.lower()))
-      attribute_id = value.lower()
 
     # Plain string assignment
     if type(attr) is str:
@@ -698,7 +749,7 @@ class Jiraclient(object):
         try:
           attr.pop(attr.index({'id':None}))
         except Exception: pass
-        attr.append({'id':str(attribute_id)})
+        setattr(issue,key,[{'id':str(attribute_id)}])
 
     if type(attr) is dict:
       #self.logger.debug("set dict attr %s %s" % (key,value))
@@ -713,6 +764,19 @@ class Jiraclient(object):
 
     return issue
 
+  def update_issue_from_options(self,issue):
+    self.logger.debug("update issue: %s" % issue)
+    for key in issue.__dict__.keys():
+      self.logger.debug("attr %s" % key)
+      if hasattr(self.options,key):
+        attr = getattr(self.options,key)
+        if attr:
+          values = getattr(self.options,key).split(',')
+          for value in values:
+            issue = self.update_issue_obj(issue,key,value)
+    self.logger.debug("updated issue: %s" % issue)
+    return issue
+
   def create_issue_obj(self,defaults=False):
     # Trigger to parse rc file for issue default values
     if defaults:
@@ -724,35 +788,23 @@ class Jiraclient(object):
     # Creates an Issue object based on CLI args and config file.
     # We do this for create and modify operations.
     issue = Issue()
+    issue = self.update_issue_from_options(issue)
+
+    if self.options.epic_theme:
+      setattr(issue,self.options.epic_theme_id,self.options.epic_theme)
+    if self.options.timetracking:
+      setattr(issue,'timetracking',{"originalEstimate": self.options.timetracking})
+    if self.options.remaining:
+      setattr(issue,'timetracking',{"remainingEstimate": self.options.remaining})
 
     # A real issue object contains stuff we've received from 
     # the Jira API, if in noop mode, don't reach out.
     if self.options.noop:
       return issue
 
-    self.get_project_id(self.options.project)
-    self.get_issue_types(self.options.project)
-    self.get_project_versions(self.options.project)
-    self.get_project_components(self.options.project)
-    self.get_resolutions()
-    self.get_priorities()
-
-    for key in issue.__dict__.keys():
-      self.logger.debug("attr %s" % key)
-      if hasattr(self.options,key):
-        attr = getattr(self.options,key)
-        if attr:
-          values = getattr(self.options,key).split(',')
-          for value in values:
-            issue = self.update_issue_obj(issue,key,value)
-
-    if self.options.epic_theme:
-      setattr(issue,self.options.epic_theme_id,self.options.epic_theme)
-
-    if self.options.timetracking:
-      issue.timetracking = { "originalEstimate": self.options.timetracking }
-    if self.options.remaining:
-      issue.timetracking = { "remainingEstimate": self.options.remaining }
+    # The not noop version updates data (eg. IDs) from jiraserver
+    self.update_maps_from_jiraserver()
+    issue = self.update_issue_from_options(issue)
 
     return issue
 
@@ -803,25 +855,23 @@ class Jiraclient(object):
     self.call_api(*args,payload=payload)
 
   def link_issues(self,issueFrom,linkType,issueTo):
-    self.logger.debug("Link %s -> %s" % (issueFrom,issueTo))
+    self.logger.debug("Link %s -> %s -> %s" % (issueFrom,linkType,issueTo))
     uri = 'rest/api/latest/issueLink'
     payload = json.dumps({"type":{"name":linkType},"inwardIssue":{"key":issueFrom},"outwardIssue":{"key":issueTo},"comment":{"body":self.options.comment}})
     result = self.call_api('post',uri,payload=payload)
     return result
 
   def unlink_issues(self,issueFrom,linkType,issueTo):
-    self.logger.debug("Unlink %s -> %s" % (issueFrom,issueTo))
-    fromIssue = self.get_issue(issueFrom)
+    self.logger.debug("Unlink %s -> %s -> %s" % (issueFrom,linkType,issueTo))
     for link in self.get_issue_links(issueFrom):
       if link["outwardIssue"]["key"] == issueTo and link["type"]["name"].lower() == linkType.lower():
         return self.delete_issue_link(link["id"])
     if linkType == "jira_subtask_link":
       # issueFrom has 'subtask' field, issueTo has 'parent' field.
       # Not sure yet if I can just delete those in a modify_issue action.
-      self.log.issue("Unlinking subtasks is currently unsupported")
+      self.logger.info("Unlinking subtasks is currently unsupported")
 
   def subtask_link(self,parent,child):
-    self.logger.debug("Apply subtask link: %s -> %s" % (parent,child))
     # issueType is always jira_subtask_link, which we get from the Jira DB:
     # mysql> select * from issuelinktype;
     # +-------+-------------------+---------------------+----------------------+--------------+
@@ -835,6 +885,73 @@ class Jiraclient(object):
     # 4 rows in set (0.00 sec)
     result = self.link_issues(parent,'jira_subtask_link',child)
     return result
+
+  def create_issues_from_template(self):
+    import yaml
+
+    if not self.options.template == "-" and not os.path.exists(self.options.template):
+      self.fatal("No such file: %s" % self.options.template)
+
+    # This isn't "real" recursion because as we get deeper the thing we represent
+    # goes from Epic to Story to Subtask, which are different datatypes in Jira.
+    try:
+      if self.options.template == "-":
+        yamldata = yaml.load(sys.stdin)
+      else:
+        yamldata = yaml.load(file(self.options.template,"r"))
+    except Exception,details:
+      self.fatal("Failed to parse YAML template: %s" % details)
+
+    if yamldata.has_key('stories'):
+      stories = yamldata.pop('stories')
+    if yamldata.has_key('subtasks'):
+      subtasks = yamldata.pop('subtasks')
+
+    # Should we use the rc file for issue defaults?
+    defaults = not self.options.norcfile
+
+    # First create the Epic so we can use its Key as epic_theme in subtasks and stories
+    epic = self.create_issue_obj(defaults=defaults)
+    for (k,v) in yamldata.items():
+      epic = self.update_issue_obj(epic,k,v)
+    eid = self.create_issue(epic)
+    self.modify_issue(eid,{self.options.epic_theme_id:eid})
+
+    # create subtasks for eid, inheriting from epic
+    for subtask in subtasks:
+      issue = epic
+      for (k,v) in subtask.items():
+        issue = self.update_issue_obj(issue,k,v)
+      issue.issuetype = {"id":self.maps['issuetype'].find_key("sub-task")}
+      issue.parent = {'key':eid}
+      stid = self.create_issue(issue)
+      #self.subtask_link(eid,stid)
+      #self.modify_issue(stid,{self.options.epic_theme_id:eid})
+      #self.modify_issue(stid,{self.options.epic_theme_id:eid,"issuetype":{"id":self.maps['issuetype'].find_key("sub-task")}})
+
+    # create stories for eid, inheriting from epic
+    for story in stories:
+      subtasks = None
+      if story.has_key('subtasks'):
+        subtasks = story.pop('subtasks')
+
+      issue = epic
+      for (k,v) in story.items():
+        issue = self.update_issue_obj(issue,k,v)
+      sid = self.create_issue(issue)
+      self.modify_issue(sid,{self.options.epic_theme_id:eid})
+
+      if subtasks:
+        # create subtasks for stories of epic, inheriting from epic
+        for subtask in subtasks:
+          issue = epic
+          for (k,v) in subtask.items():
+            issue = self.update_issue_obj(issue,k,v)
+          stid = self.create_issue(issue)
+          self.subtask_link(sid,stid)
+          self.modify_issue(stid,{self.options.epic_theme_id:eid})
+
+    self.logger.info("Created %s/browse/%s" % (self.get_serverinfo()['baseUrl'], eid))
 
   def act_on_existing_issue(self):
 
@@ -898,11 +1015,26 @@ class Jiraclient(object):
 
     # Run a named Jira API call and return
     if self.options.api is not None:
+      payload = None
+      if self.options.jsondata:
+        if os.path.exists(self.options.jsondata):
+          fd = open(self.options.jsondata,'r')
+          try:
+            jsondata = fd.read()
+          except Exception,msg:
+            self.fatal("Error reading jsondata: %s" % msg)
+          fd.close()
+          try:
+            payload = json.dumps(json.loads(jsondata))
+          except Exception,msg:
+            self.fatal("Error parsing jsondata: %s" % msg)
+        else:
+          self.fatal("API error: file not found: %s" % self.options.jsondata)
       try:
-        data = self.call_api('get',self.options.api)
+        response = self.call_api(self.options.method,self.options.api,payload=payload)
+        pp.pprint(response)
       except Exception, details:
         self.fatal("API error: bad method: %s" % details)
-      pp.pprint(data)
       return
 
     # Link two existing IDs
@@ -915,9 +1047,9 @@ class Jiraclient(object):
       (fromId,linktype,toId) = self.options.unlink.split(',')
       return self.unlink_issues(fromId,linktype,toId)
 
-    ## Create a set of Issues based on a YAML project file
-    #if self.options.template is not None:
-    #  return self.create_issues_from_template()
+    # Create a set of Issues based on a YAML project file
+    if self.options.template is not None:
+      return self.create_issues_from_template()
 
     # Act on an existing Jira Issue
     if self.options.issueID is not None:
