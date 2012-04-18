9d7ea51c29bd7393a7a0aa62398580fb6d23ab3a
Matt Callaway <mcallawa@genome.wustl.edu>
2012-04-18 18:35:06 -0500

    Add files

---
+++ test/DictDiffer.py
@@ -0,0 +1,40 @@
+import pprint
+pp = pprint.PrettyPrinter()
+class DictDiffer(object):
+    """
+    Calculate the difference between two dictionaries as:
+    (1) items added
+    (2) items removed
+    (3) keys same in both but changed values
+    (4) keys same in both and unchanged values
+    """
+    def __init__(self, current_dict, past_dict):
+      self.current_dict, self.past_dict = current_dict, past_dict
+      self.set_current, self.set_past = set(current_dict.keys()), set(past_dict.keys())
+      self.intersect = self.set_current.intersection(self.set_past)
+    def added(self):
+      return set(o for o in self.set_current - self.intersect)
+    def removed(self):
+      return set(o for o in self.set_past - self.intersect)
+    def changed(self):
+      return set(o for o in self.intersect if self.past_dict[o] != self.current_dict[o])
+    def unchanged(self):
+      return set(o for o in self.intersect if self.past_dict[o] == self.current_dict[o])
+    def areEqual(self):
+      ch = self.changed()
+      ch = ch.union(self.removed(),self.added())
+      if len(ch) != 0:
+        print "Differences: %s" % (ch)
+        print "Changed: %s" % (self.changed())
+        print "Added: %s" % (self.added())
+        print "Removed: %s" % (self.removed())
+        for item in ch:
+          if self.current_dict.has_key(item) and self.past_dict.has_key(item):
+            print "%s got %s vs %s" % (item,self.current_dict[item],self.past_dict[item])
+          elif self.current_dict.has_key(item) and not self.past_dict.has_key(item):
+            print "%s got %s vs %s" % (item,self.current_dict[item],None)
+          elif not self.current_dict.has_key(item) and self.past_dict.has_key(item):
+            print "%s got %s vs %s" % (item,None,self.past_dict[item])
+        return False
+      return True
+
---
+++ test/testTemplate.py
@@ -0,0 +1,150 @@
+
+import pprint
+import sys
+import os
+import logging
+import unittest
+from DictDiffer import DictDiffer;
+
+if os.path.exists("./jiraclient/"):
+  sys.path.insert(0,"./jiraclient/")
+
+import jiraclient
+
+pp = pprint.PrettyPrinter(depth=4,stream=sys.stdout)
+
+class TestUnit(unittest.TestCase):
+
+  def setUp(self):
+    self.c = jiraclient.Jiraclient()
+    self.c.parse_args()
+    self.c.options.nopost = True
+    self.c.options.norcfile = False
+    self.c.options.config = "./test/data/jiraclientrc-001"
+    self.c.options.sessionfile = "./test/data/jira-session"
+    self.c.options.loglevel = "DEBUG"
+    self.c.prepare_logger()
+    self.c.read_config()
+    self.c.options.user = 'jirauser'
+    self.c.options.password = 'jirauser'
+
+  def testTemplate000(self):
+    self.c.options.template = "./test/data/project-000.yaml"
+    self.c.create_issues_from_template()
+    desired = {
+     'assignee': {'name': 'jirauser'},
+     'components': [{'id': '10111'}],
+     'customfield_10010': ['NOOP'],
+     'description': 'Epic description',
+     'fixVersions': [{'id': '10020'}],
+     'issuetype': {'id': '6'},
+     'priority': {'id': '6'},
+     'project': {'id': '10001'},
+     'summary': 'This is an Epic'
+     }
+    for got in self.c.issues_created:
+      diff = DictDiffer(got,desired)
+      assert diff.areEqual()
+
+  def testTemplate001(self):
+    self.c.options.template = "./test/data/project-001.yaml"
+    self.c.create_issues_from_template()
+    desired_epic = {
+     'assignee': {'name': 'jirauser'},
+     'components': [{'id': '10111'}],
+     'customfield_10010': ['NOOP'],
+     'description': 'Test Epic description',
+     'fixVersions': [{'id': '10020'}],
+     'issuetype': {'id': '6'},
+     'priority': {'id': '6'},
+     'project': {'id': '10001'},
+     'summary': 'This is a test Epic'
+    }
+    desired_subtask = {
+     'assignee': {'name': 'jirauser'},
+     'components': [{'id': '10111'}],
+     'customfield_10010': ['NOOP'],
+     'fixVersions': [{'id': '10020'}],
+     'issuetype': {'id': '3'},
+     'priority': {'id': '6'},
+     'project': {'id': '10001'},
+     'summary': 'This is test epic subtask 1',
+     'description': 'This is test epic subtask 1 description'
+    }
+    epic = self.c.issues_created[0]
+    diff = DictDiffer(epic,desired_epic)
+    assert diff.areEqual()
+    subtask = self.c.issues_created[1]
+    diff = DictDiffer(subtask,desired_subtask)
+    assert diff.areEqual()
+
+  def testTemplate002(self):
+    self.c.options.template = "./test/data/project-002.yaml"
+    self.c.create_issues_from_template()
+    desired_epic = {
+     'assignee': {'name': 'jirauser'},
+     'components': [{'id': '10111'}],
+     'customfield_10010': ['NOOP'],
+     'description': 'Epic description',
+     'fixVersions': [{'id': '10020'}],
+     'issuetype': {'id': '6'},
+     'priority': {'id': '6'},
+     'project': {'id': '10001'},
+     'summary': 'This is an Epic'
+    }
+    desired_subtask = {
+     'assignee': {'name': 'jirauser'},
+     'components': [{'id': '10111'}],
+     'customfield_10010': ['NOOP'],
+     'fixVersions': [{'id': '10020'}],
+     'issuetype': {'id': '3'},
+     'priority': {'id': '6'},
+     'project': {'id': '10001'},
+     'summary': 'est1 summary',
+    }
+    desired_story = {
+     'assignee': {'name': 'jirauser'},
+     'components': [{'id': '10111'}],
+     'customfield_10010': ['NOOP'],
+     'fixVersions': [{'id': '10020'}],
+     'issuetype': {'id': '7'},
+     'priority': {'id': '6'},
+     'project': {'id': '10001'},
+     'summary': 's1 summary',
+     'description': 'story s1 description'
+    }
+    desired_story_subtask = {
+     'assignee': {'name': 'jirauser'},
+     'components': [{'id': '10111'}],
+     'customfield_10010': ['NOOP'],
+     'fixVersions': [{'id': '10020'}],
+     'issuetype': {'id': '3'},
+     'priority': {'id': '6'},
+     'project': {'id': '10001'},
+     'summary': 's1 st1 summary',
+     'description': 's1 st1 description'
+    }
+    epic = self.c.issues_created[0]
+    diff = DictDiffer(epic,desired_epic)
+    assert diff.areEqual()
+    subtask = self.c.issues_created[1]
+    diff = DictDiffer(subtask,desired_subtask)
+    assert diff.areEqual()
+    story = self.c.issues_created[2]
+    diff = DictDiffer(story,desired_story)
+    assert diff.areEqual()
+    subtask = self.c.issues_created[3]
+    diff = DictDiffer(subtask,desired_story_subtask)
+    assert diff.areEqual()
+
+def suite():
+  suite = unittest.makeSuite(TestUnit,'test')
+  suite = unittest.TestSuite()
+  #suite.addTest(TestUnit("testTemplate000"))
+  #suite.addTest(TestUnit("testTemplate001"))
+  suite.addTest(TestUnit("testTemplate002"))
+
+  return suite
+
+if __name__ == "__main__":
+  unittest.main(defaultTest="suite")
