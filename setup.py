9c06cb5120c58fcc9c71e1e5e26da8091758fef1
Matt Callaway <mcallawa@genome.wustl.edu>
2014-01-21 09:32:09 -0600

    Require restkit

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 setup(name='jiraclient',
       packages=['jiraclient'],
       scripts=['bin/jiraclient'],
+      install_requires=[
+          "restkit"
+      ],
      )
 
